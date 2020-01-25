ccdba7288165d348aa8d20ba8a3bd319da5c0d73
Joy Karmoker <joykarmoker353@gmail.com>
2020-01-25 12:30:12 +0600

    Player Moving Script Added

---
+++ Assets/Scriptes/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+        var newXPos = transform.position.x + deltaX;
+
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
+        var newYPos = transform.position.y + deltaY;
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,9 @@ This is classical unity 2D game where there is a fight spaceships.
 Player trys to kill the enemy spaceships with lase bullets.
 
 Commit-1: Created The Repo - This is the start point for The Project
+
+commit-2: Setted up The Project - Created the background, set up the canvas,
+and added player and enemy sprite in the scene
+
+commit-3: Player Moving Script Added - Added Player Script in the player Object so it can move
+on press of up, down, left and right key.
