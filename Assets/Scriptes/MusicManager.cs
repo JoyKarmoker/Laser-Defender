24148aa569f6a9a48df675c590deac4deb1abc96
Joy Karmoker <joykarmoker353@gmail.com>
2020-02-02 20:58:44 +0600

    Music Manager added but not working in the first scene

---
+++ Assets/Scriptes/MusicManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] musicChangeArray;
+    private AudioSource audioSource;
+    
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("Don't Destroy On load " + name);
+    }
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+       
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = musicChangeArray[level];
+        Debug.Log("Playing AudioClip: " + thisLevelMusic);
+        if(thisLevelMusic)
+        {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+    }
+}
