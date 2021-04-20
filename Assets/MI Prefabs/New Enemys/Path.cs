9113ab0ad4b956864894aad805bde3c99898f435
Joy Karmoker <Joy Karmoker353@gmail.com>
2021-04-20 23:34:25 +0600

    Created new curved paths for enemy

--- Assets/MI Prefabs/New Enemys/Path.cs
+++ Assets/MI Prefabs/New Enemys/Path.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Path : MonoBehaviour
 {
+    [Range(1, 20)] public int curvedPathDensity = 2;
+    int overload;
     Transform[] pathPointArray;
-
     public List<Transform> pathPointList = new List<Transform>();
+    public List<Vector2> curvedPathPointList = new List<Vector2>();
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -51,5 +55,43 @@ public class Path : MonoBehaviour
             }
         }
 
+        //Curved Path
+
+        //Check Overload
+        if(pathPointList.Count %2 == 0)
+        {
+            pathPointList.Add(pathPointList[pathPointList.Count - 1]);
+            overload = 2;
+        }
+        else
+        {
+            pathPointList.Add(pathPointList[pathPointList.Count - 1]);
+            pathPointList.Add(pathPointList[pathPointList.Count - 1]);
+            overload = 3;
+        }
+
+        //Creating Curve Path Point List
+        curvedPathPointList.Clear();
+        Vector2 lineStart = pathPointList[0].position;
+        for (int i = 0; i < pathPointList.Count - overload; i=i+2)
+        {
+            for(int j=0; j<=curvedPathDensity; j++)
+            {
+                Vector2 lineEnd = GetPoint(pathPointList[i].position, pathPointList[i + 1].position, pathPointList[i + 2].position, j / (float)curvedPathDensity);
+             
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(lineStart, lineEnd);
+
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(lineStart, 0.1f);
+                lineStart = lineEnd;
+                curvedPathPointList.Add(lineStart);
+            }
+        }
+    }
+
+    Vector2 GetPoint(Vector2 p0, Vector2 p1, Vector2 p2, float t)
+    {
+        return Vector2.Lerp(Vector2.Lerp(p0, p1, t), Vector2.Lerp(p1, p2, t), t);
     }
 }
